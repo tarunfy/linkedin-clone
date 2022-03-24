7a583164237aaa61db5e0eb8e8e26a02e4e6cac2
tarunfy <tarunsharma8920@gmail.com>
2022-03-24 16:36:11 +0530

    implemented dark mode theme

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import HeaderLink from "./HeaderLink";
 import GroupIcon from "@mui/icons-material/Group";
@@ -9,20 +10,38 @@ import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
 import { Avatar } from "@mui/material";
 import Image from "next/image";
 import React from "react";
-import logo from "../public/logo2.svg";
+import logoLight from "../public/logoLight.svg";
+import logoDark from "../public/logoDark.svg";
+import { useTheme } from "next-themes";
 
 const Header = () => {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme, theme } = useTheme();
+
+  //   After mounting, we have access to the theme:
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
-    <header className="flex justify-around items-center">
+    <header className="flex justify-around items-center py-1.5 px-3 focus-within:shadow-lg sticky top-0 z-40 bg-white dark:bg-[#1D2226]">
       {/* left */}
       <section className="flex items-center space-x-2 w-full max-w-xs">
-        <Image src={logo} width={45} height={45} />
-        <div className="flex space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-4">
+        {mounted && (
+          <>
+            {resolvedTheme === "dark" ? (
+              <Image src={logoLight} width={45} height={45} />
+            ) : (
+              <Image src={logoDark} width={55} height={55} />
+            )}
+          </>
+        )}
+        <div className="flex space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
           <SearchRoundedIcon />
           <input
             type="text"
             placeholder="Search"
-            className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder:text-black/70 dark:placeholder:text-white/75 flex-grow"
+            className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"
           />
         </div>
       </section>
